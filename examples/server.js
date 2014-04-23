2167b5ac6f7502245288b32fcadffb2825156951
Dmitry Mazuro <dmitry.mazuro@icloud.com>
2014-04-23 23:12:35 +0300

    add server example

---
+++ examples/server.js
@@ -0,0 +1,24 @@
+var amazon = require('../'),
+  koa = require('koa'),
+  router = require('koa-router');
+
+var app = koa();
+app.use(router(app));
+
+
+var client = amazon.createClient({
+  awsTag: process.env.AWS_TAG,
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET
+});
+
+
+app.get('/amazon/:index', function* (){
+  this.body = yield client.itemSearch({
+    keywords: this.query.title,
+    searchIndex: this.params.index,
+    responseGroup: 'ItemAttributes,Offers,Images'
+  });
+});
+
+app.listen(3000);
