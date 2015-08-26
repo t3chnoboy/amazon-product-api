f93e41503fd07fe66c7cb11622679dc0caed5ebe
Kauê Gimenes <kauegimenes@users.noreply.github.com>
2015-08-26 08:50:38 -0300

    BrowseNodeLookup example

---
+++ examples/browseNodeLookup.js
@@ -0,0 +1,17 @@
+var amazon = require('../lib');
+
+var client = amazon.createClient({
+  awsTag: process.env.AWS_TAG,
+  awsId: process.env.AWS_ID,
+  awsSecret: process.env.AWS_SECRET
+});
+
+client.itemLookup({
+  browseNodeId: '549726',
+  responseGroup: 'NewReleases'
+}, function(err, results) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(results);
+  }
