a5f6a828e502bf636ed8d2a8dd1342f1d19281a1
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2017-07-04 21:10:24 -0500

    Minor changes in favor of the airbnb eslint guide
    - change airbnb guide dependency to es5 version
    - minor refactor in the js files
    - add eslintignore file

--- examples/browseNodeLookup.js
+++ examples/browseNodeLookup.js
@@ -3,16 +3,16 @@ var amazon = require('../lib');
 var client = amazon.createClient({
   awsTag: process.env.AWS_TAG,
   awsId: process.env.AWS_ID,
-  awsSecret: process.env.AWS_SECRET,
+  awsSecret: process.env.AWS_SECRET
 });
 
 client.browseNodeLookup({
   browseNodeId: '549726',
-  responseGroup: 'NewReleases',
-}, function (err, results) {
+  responseGroup: 'NewReleases'
+}, function browseNodeLookupCallback(err, results) {
   if (err) {
-    console.log(err);
+    console.log(err); //eslint-disable-line
   } else {
-    console.log(results);
+    console.log(results); //eslint-disable-line
   }
 });
